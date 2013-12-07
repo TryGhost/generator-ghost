27e269664e2b35fa8c5078917b89cfad4e1ff68d
Jacob Gable <jacob.gable@gmail.com>
2013-12-07 15:28:39 -0600

    Use Github API for listing releases to download
    Closes #3
    
    - Add octonode dependency
    - Add github.js for fetching github releases
    - Modify app generator to fetch releases and list them
    - Fix tests to actually test installation of files

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var util = require('util');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var yeoman = require('yeoman-generator');
+var githubReleases = require('./github');
 
 
 var GhostGenerator = module.exports = function GhostGenerator(args, options, config) {
@@ -18,22 +19,38 @@ var GhostGenerator = module.exports = function GhostGenerator(args, options, con
 util.inherits(GhostGenerator, yeoman.generators.Base);
 
 GhostGenerator.prototype.askFor = function askFor() {
+  var self = this;
   var cb = this.async();
 
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    type: 'input',
-    name: 'version',
-    message: 'What version of Ghost do you want to download?',
-    default: '0.3.3'
-  }];
+  // Grab the github releases for Ghost
+  githubReleases.fetch('TryGhost', 'Ghost', function (err, releases) {
+    // Store the available release map for later.
+    self.availableReleases = releases;
 
-  this.prompt(prompts, function (props) {
-    this.version = props.version;
-    cb();
-  }.bind(this));
+    // Store the release names as the choices
+    var choices = [];
+    for(var key in releases) {
+      if (!releases.hasOwnProperty(key)) { continue; }
+
+      choices.push(key);
+    }
+
+    var prompts = [{
+      type: 'list',
+      name: 'version',
+      message: 'What version of Ghost do you want to download?',
+      choices: choices,
+      default: '0.3.3'
+    }];
+
+    self.prompt(prompts, function (props) {
+      self.version = props.version;
+      cb();
+    });
+  });
 };
 
 GhostGenerator.prototype.app = function app() {
